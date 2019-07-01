d576838a6b0976eab4548306d2d34d7a9bbdb8d5
Magiczny Czarodziej <nexit07@gmail.com>
2019-07-01 12:43:48 +0200

    Create a router

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
+import routes from './api';
 
 const app = express();
 
+app.use('/', routes);
 app.get('/', (req, res) => {
-  res.send({
-    'status': 'running',
-  })
+  res.status(200).send({
+    status: 'running',
+  });
 });
 
 app.listen(3000, () => {
