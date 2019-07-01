7e2d2d9ab430604803e0a8b7d6075d6b829cc97b
Magiczny Czarodziej <nexit07@gmail.com>
2019-07-01 23:38:41 +0200

    Add registration fields validation

--- Server/src/api/routes/auth.js
+++ Server/src/api/routes/auth.js
@@ -1,9 +1,24 @@
+import { checkSchema, validationResult } from 'express-validator';
 import AuthService from '../../services/auth';
+import { registrationSchema } from '../validationSchemas';
 
 export default (app) => {
-  app.post('/register', async (req, res) => {
+  app.post('/register', checkSchema(registrationSchema), async (req, res) => {
+    // Credentials validation
+    try {
+      validationResult(req).throw();
+    } catch (error) {
+      res.status(422).send({
+        error: {
+          code: 'FIELDS_VALIDATION_ERROR',
+          message: 'One or more fields raised validation errors',
+        }
+      });
+      return;
+    }
+    
+    // If credentials OK, proceed to registration
     const { username, password } = req.body;
-
     try {
       const user = await AuthService.register({ username, password });
       res.status(400).send({
---
+++ Server/src/api/validationSchemas.js
@@ -0,0 +1,30 @@
+const registrationSchema = {
+  username: {
+    in: 'body',
+    isString: {
+      errorMessage: 'Username must be a string',
+    },
+    isAlphanumeric: {
+      errorMessage: 'Username must be alphanumeric',
+    },
+    isLength: {
+      errorMessage: 'Username must be at least 6 characters long',
+      options: { min: 6 },
+    },
+  },
+  password: {
+    in: 'body',
+    isString: {
+      errorMessage: 'Password must be a string',
+    },
+    isAlphanumeric: {
+      errorMessage: 'Password must be alphanumeric',
+    },
+    isLength: {
+      errorMessage: 'Password must be at least 6 characters long',
+      options: { min: 6 },
+    },
+  },
+};
+
+export { registrationSchema };
